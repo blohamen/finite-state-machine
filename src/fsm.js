228086d4b45af0db1e6a977b97ecabac23a682ea
blohamen <staver82@gmail.com>
2017-10-01 20:56:16 +0300

    Update fsm.js

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,51 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        if(!config) throw new Error;
+        this.config = config;
+        this.currentState = config.initial;
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.currentState;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        if(!(state in this.config.states)) throw new Error;
+        for(var key in this.config.states){
+            if(key === state){ this.currentState = key;}
+        }
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+
+    	for(var key in this.config.states){
+		 var a = this.config.states[key].transitions;
+		 for(var key2 in a){
+		 	if(key2 === event) {this.currentState = a[key2]}
+			}
+		}		
+	 }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.currentState = this.config.initial;
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,7 +55,23 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+    	var b = [];
+    	if(!event){
+    		for(var key in this.config.states){
+    			b.push(key);
+    		}
+    		return b;
+    	}
+    	if(!(event in this.config.states)) return [];
+    	for(var key in this.config.states){
+    		var a = this.config.states.transitions;
+    		for (var key2 in a){
+    			if (key2 === event){ b.push(a[key2]);}
+    		}
+    	}
+    	return b;
+    }
 
     /**
      * Goes back to previous state.
